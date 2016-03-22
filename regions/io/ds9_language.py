eb9a322911d8b562173c48d83130c5b15a0e52c5
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2016-03-22 14:17:39 -0700

    pseudocode parser

--- regions/io/ds9_language.py
+++ regions/io/ds9_language.py
@@ -1,4 +1,17 @@
-{'point': (coordinate, coordinate),
+language = {'point': (coordinate, coordinate),
  'circle': (coordinate, coordinate, radius),
- 'box': (coordinate, coordinate, width, height),
+ 'box': (coordinate, coordinate, width, height, angle),
 }
+
+circle(1.5, 3.6, 1.2)
+
+def line_parser(line):
+    region_type = 'circle' if 'circle' in line
+
+    typer_parser(coordinate_string, language[region_type])
+
+def type_parser(string, specfication):
+    coord_list = []
+    splitter = re.compile("[, ]")
+    for element, element_parser in zip(splitter.split(string, specification):
+        coord_list.append(element_parser(element))
