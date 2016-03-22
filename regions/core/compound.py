c83c9f794495f1465ef063d28b516a3536dbd7c0
Thomas Robitaille <thomas.robitaille@gmail.com>
2016-03-22 16:39:58 -0700

    Fix syntax

--- regions/core/compound.py
+++ regions/core/compound.py
@@ -1,7 +1,7 @@
-from .core import Region
+from .core import PixelRegion, SkyRegion
 
 
-class CompoundPixelRegion(PixelRegion)
+class CompoundPixelRegion(PixelRegion):
     """
     Represents the logical combination of two regions in pixel coordinates.
     """
@@ -20,8 +20,11 @@ class CompoundPixelRegion(PixelRegion)
     def to_sky(self, wcs, mode='local', tolerance=None):
         raise NotImplementedError("")
 
+    def __repr__(self):
+        return "({0} {1} {2})".format(self.region1, self.operator, self.region2)
 
-class CompoundSkyRegion(PixelRegion)
+
+class CompoundSkyRegion(SkyRegion):
     """
     Represents the logical combination of two regions in sky coordinates.
     """
@@ -36,3 +39,6 @@ class CompoundSkyRegion(PixelRegion)
 
     def to_pixel(self, wcs, mode='local', tolerance=None):
         raise NotImplementedError("")
+
+    def __repr__(self):
+        return "({0} {1} {2})".format(self.region1, self.operator, self.region2)
