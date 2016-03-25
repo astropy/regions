f62350739b4e8b0e94bc3b90446a74a2cf27ba5c
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2016-03-25 16:33:27 -0700

    add point region

---
+++ regions/shapes/point.py
@@ -0,0 +1,70 @@
+import math
+
+import numpy as np
+from astropy import units as u
+
+from ..core import PixelRegion, SkyRegion
+
+
+class PointPixelRegion(PixelRegion):
+    """
+    A point position in pixel coordinates.
+
+    Parameters
+    ----------
+    center : :class:`~regions.core.pixcoord.PixCoord`
+        The position of the point
+    """
+
+    def __init__(self, center, meta=None, visual=None):
+        # TODO: test that center is a 0D PixCoord
+        self.center = center
+        self.meta = meta or {}
+        self.visual = visual or {}
+
+    @property
+    def area(self):
+        return 0
+
+    def __contains__(self, pixcoord):
+        return False
+
+    def to_shapely(self):
+        return self.center.to_shapely()
+
+    def to_sky(self, wcs, mode='local', tolerance=None):
+        # TOOD: needs to be implemented
+        raise NotImplementedError("")
+
+    def to_mask(self, mode='center'):
+        # TOOD: needs to be implemented
+        raise NotImplementedError("")
+
+
+
+class PixelSkyRegion(SkyRegion):
+    """
+    A pixel region in sky coordinates.
+
+    Parameters
+    ----------
+    center : :class:`~astropy.coordinates.SkyCoord`
+        The position of the point
+    """
+
+    def __init__(self, center, meta=None, visual=None):
+        # TODO: test that center is a 0D SkyCoord
+        self.center = center
+        self.meta = meta or {}
+        self.visual = visual or {}
+
+    @property
+    def area(self):
+        return 0
+
+    def __contains__(self, skycoord):
+        return False
+
+    def to_pixel(self, wcs, mode='local', tolerance=None):
+        # TOOD: needs to be implemented
+        raise NotImplementedError("")
