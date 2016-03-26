0b59136bced4622e075443f3d038aa50a8c5e2b9
Johannes King <johannes.king@mpi-hd.mpg.de>
2016-03-26 03:21:27 +0100

    change readthedocs theme

--- docs/conf.py
+++ docs/conf.py
@@ -154,3 +154,15 @@ if eval(setup_cfg.get('edit_on_github')):
 
     edit_on_github_source_root = ""
     edit_on_github_doc_root = "docs"
+
+
+# on_rtd is whether we are on readthedocs.org
+import os
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+
+if not on_rtd:  # only import and set the theme if we're building docs locally
+    import sphinx_rtd_theme
+    html_theme = 'sphinx_rtd_theme'
+    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+
+# otherwise, readthedocs.org uses their theme by default, so no need to specify it
