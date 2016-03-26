c8bdc77caaae25378b869f49cd3fabb8be43df3a
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2016-03-26 14:27:22 -0700

    just some profiling notes

--- regions/io/tests/profile_ds9_parser.py
+++ regions/io/tests/profile_ds9_parser.py
@@ -1,10 +1,11 @@
 from regions.io import read_ds9
 import glob
+import time
 
 l = glob.glob('data/*.reg')
 
-from regions.io.read_ds9 import ds9_parser, line_parser, region_list_to_objects
-# %lprun -f ds9_parser -f line_parser -f region_list_to_objects read_all_test_files()
+from regions.io.read_ds9 import ds9_parser, line_parser, region_list_to_objects, type_parser, coordinate, angular_length_quantity
+# %lprun -f type_parser -f ds9_parser -f line_parser -f region_list_to_objects -f coordinate -f angular_length_quantity read_all_test_files()
 
 
 def read_all_test_files():
@@ -13,4 +14,6 @@ def read_all_test_files():
         if 'strip' in f or 'comment' in f:
             continue
            
+        t0 = time.time()
         read_ds9(str(f))
+        print(time.time()-t0)
