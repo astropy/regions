a028a826e1f83bfd5c52ce28d7548ea5a11d6b5a
Brigitta Sipocz <bsipocz@gmail.com>
2016-03-27 14:06:54 -0700

    Cleaning up non dependencies from conftest

--- regions/conftest.py
+++ regions/conftest.py
@@ -14,6 +14,8 @@ enable_deprecations_as_exceptions()
 try:
     PYTEST_HEADER_MODULES['Astropy'] = 'astropy'
     del PYTEST_HEADER_MODULES['h5py']
+    del PYTEST_HEADER_MODULES['Scipy']
+    del PYTEST_HEADER_MODULES['Pandas']
 except KeyError:
     pass
 
