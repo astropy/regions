3db128a17837883bee6065a006b9888be46f6d3f
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2016-03-26 18:01:13 -0700

    refactor writer to use classes for simplicity

--- regions/io/write_ds9.py
+++ regions/io/write_ds9.py
@@ -1,6 +1,7 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 
 from astropy import coordinates
+from .. import shapes
 
 coordsys_name_mapping = dict(zip(coordinates.frame_transform_graph.get_names(),
                                  coordinates.frame_transform_graph.get_names()))
@@ -10,12 +11,12 @@ coordsys_name_mapping['ecliptic'] = 'geocentrictrueecliptic' # needs expert atte
 def objects_to_ds9_string(obj_list, coordsys='fk5', fmt='.4f', radunit='deg'):
     """Take a list of regions and generate ds9 region strings"""
 
-    ids = {"<class 'regions.shapes.circle.CircleSkyRegion'>": 'skycircle',
-           "<class 'regions.shapes.circle.CirclePixelRegion'>": 'pixcircle',
-           "<class 'regions.shapes.ellipse.EllipseSkyRegion'>": 'skyellipse',
-           "<class 'regions.shapes.ellipse.EllipsePixelRegion'>": 'pixellipse',
-           "<class 'regions.shapes.polygon.PolygonSkyRegion'>": 'skypolygon',
-           "<class 'regions.shapes.polygon.PolygonPixelRegion'>": 'pixpolygon'}
+    ids = {shapes.circle.CircleSkyRegion: 'skycircle',
+           shapes.circle.CirclePixelRegion: 'pixcircle',
+           shapes.ellipse.EllipseSkyRegion: 'skyellipse',
+           shapes.ellipse.EllipsePixelRegion: 'pixellipse',
+           shapes.polygon.PolygonSkyRegion: 'skypolygon',
+           shapes.polygon.PolygonPixelRegion: 'pixpolygon'}
 
     if radunit == 'arcsec':
         if coordsys in coordsys_name_mapping.keys():
@@ -32,51 +33,55 @@ def objects_to_ds9_string(obj_list, coordsys='fk5', fmt='.4f', radunit='deg'):
     output = '# Region file format: DS9 astropy/regions\n'
     output += '{}\n'.format(coordsys)
 
+    # convert coordsys string to coordsys object
+    if coordsys in coordsys_name_mapping:
+        frame = coordinates.frame_transform_graph.lookup_name(coordsys_name_mapping[coordsys])
+    else:
+        # for pixel/image/physical frames
+        frame = None
+
     for reg in obj_list:
-        temp = str(reg.__class__)
-        if temp in ids.keys():
-            t = ids[temp]
-            if t == 'skycircle':
-                # TODO: Why is circle.center a list of SkyCoords?
-                x = reg.center.transform_to(coordsys).spherical.lon.to('deg').value[0]
-                y = reg.center.transform_to(coordsys).spherical.lat.to('deg').value[0]
-                r = reg.radius.to(radunit).value
-                output += ds9_strings['circle'].format(**locals())
-            elif t == 'pixcircle':
-                # TODO: Why is circle.center a list of SkyCoords?
-                x = reg.center.x
-                y = reg.center.y
-                r = reg.radius
-                output += ds9_strings['circle'].format(**locals())
-            elif t == 'skyellipse':
-                x = reg.center.transform_to(coordsys).spherical.lon.to('deg').value[0]
-                y = reg.center.transform_to(coordsys).spherical.lat.to('deg').value[0]
-                r2 = reg.major.to(radunit).value
-                r1 = reg.minor.to(radunit).value
-                ang = reg.angle.to('deg').value
-                output += ds9_strings['ellipse'].format(**locals())
-            elif t == 'pixellipse':
-                x = reg.center.x
-                y = reg.center.y
-                r2 = reg.major
-                r1 = reg.minor
-                ang = reg.angle
-                output += ds9_strings['ellipse'].format(**locals())
-            elif t == 'skypolygon':
-                v = reg.vertices.transform_to(coordsys)
-                coords = [(x.to('deg').value, y.to('deg').value) for x, y in
-                          zip(v.spherical.lon, v.spherical.lat)]
-                val = "{:"+fmt+"}"
-                temp = [val.format(x) for _ in coords for x in _]
-                c = ",".join(temp)
-                output += ds9_strings['polygon'].format(**locals())
-            elif t == 'pixpolygon':
-                v = reg.vertices
-                coords = [(x, y) for x, y in zip(v.x, v.y)]
-                val = "{:"+fmt+"}"
-                temp = [val.format(x) for _ in coords for x in _]
-                c = ",".join(temp)
-                output += ds9_strings['polygon'].format(**locals())
+        if isinstance(reg, shapes.circle.CircleSkyRegion):
+            # TODO: Why is circle.center a list of SkyCoords?
+            x = reg.center.transform_to(frame).spherical.lon.to('deg').value[0]
+            y = reg.center.transform_to(frame).spherical.lat.to('deg').value[0]
+            r = reg.radius.to(radunit).value
+            output += ds9_strings['circle'].format(**locals())
+        elif isinstance(reg, shapes.circle.CirclePixelRegion):
+            # TODO: Why is circle.center a list of SkyCoords?
+            x = reg.center.x
+            y = reg.center.y
+            r = reg.radius
+            output += ds9_strings['circle'].format(**locals())
+        elif isinstance(reg, shapes.ellipse.EllipseSkyRegion):
+            x = reg.center.transform_to(frame).spherical.lon.to('deg').value[0]
+            y = reg.center.transform_to(frame).spherical.lat.to('deg').value[0]
+            r2 = reg.major.to(radunit).value
+            r1 = reg.minor.to(radunit).value
+            ang = reg.angle.to('deg').value
+            output += ds9_strings['ellipse'].format(**locals())
+        elif isinstance(reg, shapes.ellipse.EllipsePixelRegion):
+            x = reg.center.x
+            y = reg.center.y
+            r2 = reg.major
+            r1 = reg.minor
+            ang = reg.angle
+            output += ds9_strings['ellipse'].format(**locals())
+        elif isinstance(reg, shapes.polygon.PolygonSkyRegion):
+            v = reg.vertices.transform_to(frame)
+            coords = [(x.to('deg').value, y.to('deg').value) for x, y in
+                      zip(v.spherical.lon, v.spherical.lat)]
+            val = "{:"+fmt+"}"
+            temp = [val.format(x) for _ in coords for x in _]
+            c = ",".join(temp)
+            output += ds9_strings['polygon'].format(**locals())
+        elif isinstance(reg, shapes.polygon.PolygonPixelRegion):
+            v = reg.vertices
+            coords = [(x, y) for x, y in zip(v.x, v.y)]
+            val = "{:"+fmt+"}"
+            temp = [val.format(x) for _ in coords for x in _]
+            c = ",".join(temp)
+            output += ds9_strings['polygon'].format(**locals())
 
     return output
 
