8a1fab5cc7f565c39e726d119e880861b0fc563c
Christoph Deil <Deil.Christoph@gmail.com>
2016-06-23 16:47:25 +0200

    Add some info to package docstring

--- regions/__init__.py
+++ regions/__init__.py
@@ -1,7 +1,13 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 
 """
-This is an experimental package for representing regions
+This is an in-development package for region handling based on Astropy.
+
+The goal is to merge the functionality from pyregion and photutils apertures
+and then after some time propose this package for inclusion in the Astropy core.
+
+* Code : https://github.com/astropy/regions
+* Docs : http://astropy-regions.readthedocs.io/en/latest/
 """
 
 # Affiliated packages may add whatever they like to this file, but
