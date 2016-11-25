ffcb9e6bbb91ee489a3ebb6adf246b1071160563
Thomas Robitaille <thomas.robitaille@gmail.com>
2016-11-25 10:04:33 +0000

    Added CircleCI configuration for 32-bit testing

---
+++ .run_docker_tests.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+docker info
+
+cat << EOF | docker run -i \
+                        -v ${PWD}:/regions_src \
+                        -a stdin -a stdout -a stderr \
+                        astropy/affiliated-32bit-test-env:1.6 \
+                        bash || exit $?
+
+cd /regions_src
+
+echo "Output of uname -m:"
+uname -m
+
+echo "Output of sys.maxsize in Python:"
+python -c 'import sys; print(sys.maxsize)'
+
+python setup.py test -V -a "-s"
+
+EOF
