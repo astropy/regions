4b9d403b4bc81c2262a267da837cebdcd95e165f
Christoph Deil <Deil.Christoph@gmail.com>
2016-11-28 20:42:55 +0100

    Remove circle sky region plot test

--- regions/shapes/tests/test_circle.py
+++ regions/shapes/tests/test_circle.py
@@ -16,17 +16,9 @@ from .utils import ASTROPY_LT_13
 
 try:
     import matplotlib
-
     HAS_MATPLOTLIB = True
 except:
     HAS_MATPLOTLIB = False
-
-try:
-    import wcsaxes
-
-    HAS_WCSAXES = True
-except:
-    HAS_WCSAXES = False
 
 
 def test_circle_pixel():
@@ -65,31 +57,6 @@ def test_transformation():
     assert_quantity_allclose(skycircle.center.data.lon, skycircle2.center.data.lon)
     assert_quantity_allclose(skycircle.center.data.lat, skycircle2.center.data.lat)
     assert_quantity_allclose(skycircle2.radius, skycircle.radius)
-
-
-# Todo : restructure test to use same circle everywhere
-# see https://github.com/astropy/regions/pull/20
-@pytest.mark.skipif('not HAS_MATPLOTLIB')
-@pytest.mark.skipif('not HAS_WCSAXES')
-def test_plot():
-    import matplotlib.pyplot as plt
-
-    skycoord = SkyCoord(1.983333 * u.deg, -1.99 * u.deg, frame='galactic')
-    c = CircleSkyRegion(skycoord, 20 * u.arcsec)
-    c.visual.update(color='red')
-    headerfile = get_pkg_data_filename('data/example_header.fits')
-    h = getheader(headerfile)
-    wcs = WCS(h)
-    fig = plt.figure()
-    ax = fig.add_axes([0.1, 0.1, 0.8, 0.8], projection=wcs)
-    p = c.as_patch(ax, alpha=0.6)
-
-    assert_allclose(p.center[0], skycoord.icrs.ra.value)
-    assert_allclose(p.center[1], skycoord.icrs.dec.value)
-    assert p.get_facecolor() == (1, 0, 0, 0.6)
-
-    c.plot(ax, alpha=0.6)
-
 
 
 def test_to_mask():
