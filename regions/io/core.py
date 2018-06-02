9e9e56b75ff35a6495bc1f6f65b77690af256846
sushobhana <sushobhanapatra@gmail.com>
2018-06-03 00:35:11 +0530

    added to_shape_list method and to_ds9 method

--- regions/io/core.py
+++ regions/io/core.py
@@ -3,18 +3,54 @@
 from __future__ import absolute_import, division, print_function
 from collections import OrderedDict
 from warnings import warn
+import copy
+import string
 
 from astropy import units as u
 from astropy import coordinates
-from astropy.coordinates import BaseCoordinateFrame
+from astropy.coordinates import BaseCoordinateFrame, SkyCoord, Angle
 from astropy import log
 
-__all__ = ['ShapeList', 'Shape']
+__all__ = ['ShapeList', 'Shape', 'to_shape_list']
 
 from .. import shapes
-from ..core import PixCoord
+from ..core import PixCoord, SkyRegion
 from .ds9.core import DS9RegionParserWarning, DS9RegionParserError
 from .crtf.core import CRTFRegionParserWarning, CRTFRegionParserError
+
+regions_attributes = dict(circle=['center', 'radius'],
+                          ellipse=['center', 'width', 'height', 'angle'],
+                          rectangle=['center', 'width', 'height', 'angle'],
+                          polygon=['vertices'],
+                          annulus=['center', 'inner_radius', 'outer_radius'],
+                          line=['start', 'end'],
+                          point=['center']
+                          )
+
+reg_mapping = {'DS9': {x: x for x in regions_attributes.keys()},
+               'CRTF': {x: x for x in regions_attributes.keys()}}
+reg_mapping['DS9']['box'] = 'rectangle'
+reg_mapping['CRTF']['rotbox'] = 'rectangle'
+reg_mapping['CRTF']['poly'] = 'polygon'
+
+
+# valid astropy coordinate frames in their respective formats.
+valid_coordsys = {'DS9': ['image', 'physical', 'fk4', 'b1950', 'fk5', 'j2000', 'icrs', 'galactic',
+                          'geocentrictrueecliptic', 'wcs'],
+                  'CRTF': ['image','j2000', 'b1950', 'galactic', 'geocentrictrueecliptic', 'supergalactic', 'icrs']
+                 }
+valid_coordsys['DS9'] += ['wcs{}'.format(x) for x in string.ascii_lowercase]
+
+# Maps astropy's coordinate frame names with their respective name in the file format.
+coordsys_mapping = {'DS9': {x: x for x in valid_coordsys['DS9']},
+                    'CRTF': {x: x for x in valid_coordsys['CRTF']}
+                    }
+coordsys_mapping['CRTF']['geocentrictrueecliptic'] = 'ecliptic'
+coordsys_mapping['CRTF']['fk5'] = 'j2000'
+coordsys_mapping['CRTF']['fk4'] = 'b1950'
+coordsys_mapping['CRTF']['supergalactic'] = 'supergal'
+
+coordsys_mapping['DS9']['geocentrictrueecliptic'] = 'ecliptic'
 
 
 class ShapeList(list):
@@ -29,11 +65,120 @@ class ShapeList(list):
                 msg = 'Skipping elliptical annulus {}'.format(shape)
                 warn(msg, DS9RegionParserWarning)
                 continue
+            if shape.region_type in ['box', 'symbol', 'text'] and shape.format_type == 'CRTF':
+                msg = 'Skipping {} {}'.format(shape.region_type, shape)
+                warn(msg, CRTFRegionParserWarning)
+                continue
             log.debug(shape)
             region = shape.to_region()
             log.debug(region)
             regions.append(region)
         return regions
+
+    def to_crtf(self):
+        pass
+
+    def to_ds9(self, fmt='.6f', radunit='deg', coordsys='fk5'):
+        """
+            Convert list of regions to ds9 region strings.
+
+            Parameters
+            ----------
+            fmt : str
+                A python string format defining the output precision.  Default is .6f,
+                which is accurate to 0.0036 arcseconds.
+
+            radunit : str
+
+            coordsys : str
+
+            Returns
+            -------
+            region_string : str
+                ds9 region string
+
+            Examples
+            --------
+            TODO
+        """
+
+        ds9_strings = {
+            'circle': 'circle({1:FMT},{2:FMT},{3:FMT}RAD)',
+            'annulus': 'annulus({1:FMT},{2:FMT},{3:FMT}RAD,{4:FMT}RAD)',
+            'ellipse': 'ellipse({1:FMT},{2:FMT},{3:FMT}RAD,{4:FMT}RAD,{5:FMT})',
+            'rectangle': 'box({1:FMT},{2:FMT},{3:FMT}RAD,{4:FMT}RAD,{5:FMT})',
+            'polygon': 'polygon({1})',
+            'point': 'point({1:FMT},{2:FMT})',
+            'line': 'line({1:FMT},{2:FMT},{3:FMT},{4:FMT})',
+        }
+
+        output = '# Region file format: DS9 astropy/regions\n'
+
+        # what's this for?
+        if radunit == 'arcsec':
+            if coordsys in coordsys_mapping['DS9'].values():
+                radunitstr = '"'
+            else:
+                raise ValueError('Radius unit arcsec not valid for coordsys {}'.format(coordsys))
+        else:
+            radunitstr = ''
+
+        for key, val in ds9_strings.items():
+            ds9_strings[key] = val.replace("FMT", fmt).replace("RAD", radunitstr)
+        for key in ds9_strings:
+            # include must be a prefix "-" or ""
+            ds9_strings[key] = "{0}" + ds9_strings[key]
+
+        output += '{}\n'.format(coordsys)
+
+        for reg in self:
+
+            # default if unspecified is that include is True, which means we
+            # pretend nothing
+            include = "-" if reg.meta.get('include') in (False, '-') else ""
+
+            meta_str = " ".join("{0}={1}".format(key, val) for key, val in
+                                reg.meta.items() if key not in ('include', 'tag', 'comment'))
+            if 'tag' in reg.meta:
+                meta_str += " " + " ".join(["tag={0}".format(tag)
+                                                for tag in reg.meta['tag']])
+            if 'comment' in reg.meta:
+                meta_str += " " + reg.meta['comment']
+
+            coord = []
+
+            if coordsys not in ['image', 'physical'] :
+                for val in reg.coord:
+                    if isinstance(val, Angle):
+                        coord.append(float(val.value))
+                    else:
+                        if radunit == '' or None:
+                            coord.append(float(val.value))
+                        else:
+                            coord.append(float(val.to(radunit).value))
+                if reg.region_type in ['ellipse', 'rectangle']:
+                    coord[-1] = float(reg.coord[-1].to('deg').value)
+            else:
+                for val in reg.coord:
+                    if isinstance(val, u.Quantity):
+                        coord.append(float(val.value))
+                    else:
+                        coord.append(float(val))
+
+            if reg.region_type == 'polygon':
+                val = "{0:" + fmt + "}"
+                temp = [val.format(x) for x in coord]
+                coord = ",".join(temp)
+                line = ds9_strings['polygon'].format(include, coord)
+            else:
+                line = ds9_strings[reg.region_type].format(include, *coord)
+
+            if meta_str.strip():
+                output += "{0} # {1} \n".format(line, meta_str)
+            else:
+                output += "{0}\n".format(line)
+
+        return output
 
 
 class Shape(object):
@@ -60,43 +205,23 @@ class Shape(object):
         Include/exclude region
     """
 
-    shape_to_sky_region = {'DS9': dict(circle=shapes.CircleSkyRegion,
-                                       ellipse=shapes.EllipseSkyRegion,
-                                       box=shapes.RectangleSkyRegion,
-                                       polygon=shapes.PolygonSkyRegion,
-                                       annulus=shapes.CircleAnnulusSkyRegion,
-                                       line=shapes.LineSkyRegion,
-                                       point=shapes.PointSkyRegion
-                                       ),
+    shape_to_sky_region = dict(circle=shapes.CircleSkyRegion,
+                               ellipse=shapes.EllipseSkyRegion,
+                               rectangle=shapes.RectangleSkyRegion,
+                               polygon=shapes.PolygonSkyRegion,
+                               annulus=shapes.CircleAnnulusSkyRegion,
+                               line=shapes.LineSkyRegion,
+                               point=shapes.PointSkyRegion
+                               )
 
-                           'CRTF': dict(circle=shapes.CircleSkyRegion,
-                                        ellipse=shapes.EllipseSkyRegion,
-                                        centerbox=shapes.RectangleSkyRegion,
-                                        rotatedbox=shapes.RectangleSkyRegion,
-                                        pol=shapes.PolygonSkyRegion,
-                                        annulus=shapes.CircleAnnulusSkyRegion,
-                                        line=shapes.LineSkyRegion,
-                                        point=shapes.PointSkyRegion)
-                           }
-    shape_to_pixel_region = {'DS9': dict(circle=shapes.CirclePixelRegion,
-                                         ellipse=shapes.EllipsePixelRegion,
-                                         box=shapes.RectanglePixelRegion,
-                                         polygon=shapes.PolygonPixelRegion,
-                                         annulus=shapes.CircleAnnulusPixelRegion,
-                                         line=shapes.LinePixelRegion,
-                                         point=shapes.PointPixelRegion
-                                         ),
-
-                             'CRTF': dict(circle=shapes.CirclePixelRegion,
-                                          ellipse=shapes.EllipsePixelRegion,
-                                          centerbox=shapes.RectanglePixelRegion,
-                                          rotatedbox=shapes.RectanglePixelRegion,
-                                          poly=shapes.PolygonPixelRegion,
-                                          annulus=shapes.CircleAnnulusPixelRegion,
-                                          line=shapes.LinePixelRegion,
-                                          point=shapes.PointPixelRegion
-                                          )
-                             }
+    shape_to_pixel_region = dict(circle=shapes.CirclePixelRegion,
+                                 ellipse=shapes.EllipsePixelRegion,
+                                 rectangle=shapes.RectanglePixelRegion,
+                                 polygon=shapes.PolygonPixelRegion,
+                                 annulus=shapes.CircleAnnulusPixelRegion,
+                                 line=shapes.LinePixelRegion,
+                                 point=shapes.PointPixelRegion
+                                 )
 
     error = {'DS9': DS9RegionParserError, 'CRTF': CRTFRegionParserError}
     warning = {'DS9': DS9RegionParserWarning, 'CRTF': CRTFRegionParserWarning}
@@ -106,13 +231,41 @@ class Shape(object):
         from . import CRTFRegionParser, DS9Parser
         self.parser = {'DS9': DS9Parser, 'CRTF': CRTFRegionParser}
 
-        self.format_type = format_type
-        self.coordsys = coordsys
-        self.region_type = region_type
+        self._format_type = format_type
+        self._coordsys = coordsys
+        self._region_type = region_type
         self.coord = coord
         self.meta = meta
         self.composite = composite
         self.include = include
+        self._validate()
+
+    @property
+    def format_type(self):
+        return self._format_type
+
+    @format_type.setter
+    def format_type(self, value):
+        self._format_type = value.upper()
+        self._validate()
+
+    @property
+    def coordsys(self):
+        return self._coordsys
+
+    @coordsys.setter
+    def coordsys(self, value):
+        self._coordsys = value.lower()
+        self._validate()
+
+    @property
+    def region_type(self):
+        return self._region_type
+
+    @region_type.setter
+    def region_type(self, value):
+        self._region_type = value.lower()
+        self._validate()
 
     def __str__(self):
         ss = self.__class__.__name__
@@ -136,7 +289,7 @@ class Shape(object):
         """
         Process list of coordinates
 
-        This mainly seaches for tuple of coordinates in the coordinate list and
+        This mainly searches for tuple of coordinates in the coordinate list and
         creates a SkyCoord or PixCoord object from them if appropriate for a
         given region type. This involves again some coordinate transformation,
         so this step could be moved to the parsing process
@@ -153,7 +306,7 @@ class Shape(object):
 
     def _convert_sky_coords(self):
         """
-        Convert to sky coords
+        Convert to sky coordinates
         """
         parsed_angles = [(x, y)
                          for x, y in zip(self.coord[:-1:2], self.coord[1::2])
@@ -203,9 +356,9 @@ class Shape(object):
                         'usetex', 'labelpos', 'labeloff', 'linewidth', 'linestyle']
 
         if isinstance(coords[0], BaseCoordinateFrame):
-            reg = self.shape_to_sky_region[self.format_type][self.region_type](*coords)
+            reg = self.shape_to_sky_region[self.region_type](*coords)
         elif isinstance(coords[0], PixCoord):
-            reg = self.shape_to_pixel_region[self.format_type][self.region_type](*coords)
+            reg = self.shape_to_pixel_region[self.region_type](*coords)
         else:
             self._raise_error("No central coordinate")
 
@@ -221,3 +374,68 @@ class Shape(object):
 
     def _raise_error(self, msg):
         raise self.error[self.format_type](msg)
+
+    def to_crtf(self):
+        """
+        Makes shape CRTF compatible.
+        """
+        self.format_type = 'CRTF'
+
+    def to_ds9(self):
+        """
+        Makes shape DS9 compatible
+        """
+        self.format_type = 'DS9'
+
+    def _validate(self):
+        """
+        Checks whether all the attributes of this object is valid according to the given format.
+        """
+        if self.format_type not in ['CRTF', 'DS9']:
+            raise ValueError("{0} is not available as io".format(self.format_type))
+        if self.region_type not in regions_attributes.keys():
+            raise ValueError("{0} is not a valid region type in {1}".format(self.region_type, self.format_type))
+        if self.coordsys not in valid_coordsys[self.format_type]:
+            raise ValueError("{0} is not a valid coordinate reference frame in {1}".
+                             format(self.coordsys, self.format_type))
+
+
+def to_shape_list(region_list, format_type='DS9', coordinate_system='fk5'):
+
+    shape_list = ShapeList()
+
+    for region in region_list:
+
+        coord = []
+
+        reg_type = str((type(region))).split(".")[2]
+        for val in regions_attributes[reg_type]:
+            coord.append(getattr(region, val))
+
+        if reg_type == 'polygon':
+            coord = [x for x in region.vertices]
+
+        coordsys = 'image'
+        if isinstance(region, SkyRegion):
+            coordsys = coord[0].name
+
+        coordsys = coordinate_system or coordsys
+        frame = coordinates.frame_transform_graph.lookup_name(coordsys)
+
+        new_coord = []
+        for val in coord:
+            if isinstance(val, Angle) or isinstance(val, u.Quantity) or isinstance(val, float):
+                new_coord.append(val)
+            elif isinstance(val, PixCoord):
+                new_coord.append(u.Quantity(val.x, u.dimensionless_unscaled))
+                new_coord.append(u.Quantity(val.y, u.dimensionless_unscaled))
+            else:
+                new_coord.append(Angle(val.transform_to(frame).spherical.lon))
+                new_coord.append(Angle(val.transform_to(frame).spherical.lat))
+
+        meta = copy.deepcopy(region.meta)
+        meta.update(region.visual)
+        shape_list.append(Shape(format_type, coordsys, reg_type, new_coord, meta, False,
+                                region.meta.get('include', False)))
+
+    return shape_list
