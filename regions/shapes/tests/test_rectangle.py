9c0b3d49f4168a0b319fb4740d579cfc4888e313
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2018-08-30 11:38:49 -0400

    test real shapes instead of sets

--- regions/shapes/tests/test_rectangle.py
+++ regions/shapes/tests/test_rectangle.py
@@ -42,14 +42,14 @@ def test_corners():
     reg = RectanglePixelRegion(center=PixCoord(xc, yc),
                                width=width, height=height, angle=90*u.deg)
     # simple case: rotate by 90
-    assert {(2.5, 3), (1.5, 3), (2.5, 1), (1.5,1)} == set(map(tuple,
-                                                              reg.corners.tolist()))
+    np.testing.assert_array_equal([(2.5, 1.), (2.5, 3.), (1.5, 3.), (1.5,1.)],
+                                  reg.corners)
 
     reg = RectanglePixelRegion(center=PixCoord(xc, yc),
                                width=width, height=height, angle=0*u.deg)
     # simpler case: rotate by 0
-    assert {(3, 2.5), (3, 1.5), (1, 2.5), (1, 1.5)} == set(map(tuple,
-                                                               reg.corners.tolist()))
+    np.testing.assert_array_equal([(1, 1.5), (3, 1.5), (3, 2.5), (1, 2.5)],
+                                  reg.corners)
 
     poly = reg.to_polygon()
     assert len(poly.vertices) == 4
