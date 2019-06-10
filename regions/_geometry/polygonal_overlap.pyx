078761693c1bc4eea39edd9dfccbf7d6abe7404f
Christoph Deil <Deil.Christoph@gmail.com>
2019-06-10 23:17:20 +0200

    Set cython: language_level=3

--- regions/_geometry/polygonal_overlap.pyx
+++ regions/_geometry/polygonal_overlap.pyx
@@ -1,5 +1,5 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
-
+# cython: language_level=3
 from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
 
