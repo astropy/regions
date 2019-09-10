94eea257428f6a8a4f967fd0e9b8c26924b7adb5
Pey Lian Lim <2090236+pllim@users.noreply.github.com>
2019-09-10 16:45:27 -0400

    Fix helpers warnings

--- docs/conf.py
+++ docs/conf.py
@@ -76,7 +76,7 @@ rst_epilog += """
 # -- Project information ------------------------------------------------------
 
 # This does not *have* to match the package name, but typically does
-project = setup_cfg['package_name']
+project = setup_cfg['name']
 author = setup_cfg['author']
 copyright = '{0}, {1}'.format(
     datetime.datetime.now().year, setup_cfg['author'])
@@ -85,8 +85,8 @@ copyright = '{0}, {1}'.format(
 # |version| and |release|, also used in various other places throughout the
 # built documents.
 
-__import__(setup_cfg['package_name'])
-package = sys.modules[setup_cfg['package_name']]
+__import__(setup_cfg['name'])
+package = sys.modules[setup_cfg['name']]
 
 # The short X.Y version.
 version = package.__version__.split('-', 1)[0]
@@ -153,7 +153,7 @@ man_pages = [('index', project.lower(), project + u' Documentation',
 if eval(setup_cfg.get('edit_on_github')):
     extensions += ['astropy_helpers.sphinx.ext.edit_on_github']
 
-    versionmod = __import__(setup_cfg['package_name'] + '.version')
+    versionmod = __import__(setup_cfg['name'] + '.version')
     edit_on_github_project = setup_cfg['github_project']
     if versionmod.version.release:
         edit_on_github_branch = "v" + versionmod.version.version
@@ -166,7 +166,7 @@ if eval(setup_cfg.get('edit_on_github')):
 github_issues_url = 'https://github.com/astropy/regions/issues/'
 
 plot_rcparams = {'savefig.bbox': 'tight',
-                 'savefig.facecolor':'none',
+                 'savefig.facecolor': 'none',
                  'axes.formatter.useoffset': False,
                  'xtick.labelsize': 9,
                  'ytick.labelsize': 9,
