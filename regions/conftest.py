7ed2a55ea2901b6afd673cb7264ae6b2608b4a3d
Thomas Robitaille <thomas.robitaille@gmail.com>
2020-05-18 17:53:22 +0100

    Refactor package infrastructure following APE17

--- regions/conftest.py
+++ regions/conftest.py
@@ -1,8 +1,11 @@
-# this contains imports plugins that configure py.test for astropy tests.
-# by importing them here in conftest.py they are discoverable by py.test
-# no matter how it is invoked within the source tree.
+# This file is used to configure the behavior of pytest when using the Astropy
+# test infrastructure. It needs to live inside the package in order for it to
+# get picked up when running the tests inside an interpreter using
+# packagename.test
 
-from distutils.version import LooseVersion
+import os
+
+from astropy.version import version as astropy_version
 
 try:
     import pytest_arraydiff
@@ -15,57 +18,45 @@ else:
     # function name.
     del pytest_arraydiff
 
-
-from astropy.version import version as astropy_version
-
-if LooseVersion(astropy_version) < LooseVersion('2.0.3'):
-    # Astropy is not compatible with the standalone plugins prior this while
-    # astroquery requires them, so we need this workaround. This will mess
-    # up the test header, but everything else will work.
-    from astropy.tests.pytest_plugins import (PYTEST_HEADER_MODULES,
-                                              TESTED_VERSIONS)
-elif astropy_version < '3.0':
-    # With older versions of Astropy, we actually need to import the pytest
-    # plugins themselves in order to make them discoverable by pytest.
-    from astropy.tests.pytest_plugins import *
+# For Astropy 3.0 and later, we can use the standalone pytest plugin
+if astropy_version < '3.0':
+    from astropy.tests.pytest_plugins import *  # noqa
+    del pytest_report_header
+    ASTROPY_HEADER = True
 else:
-    # As of Astropy 3.0, the pytest plugins provided by Astropy are
-    # automatically made available when Astropy is installed. This means it's
-    # not necessary to import them here, but we still need to import global
-    # variables that are used for configuration.
-    from astropy.tests.plugins.display import PYTEST_HEADER_MODULES, TESTED_VERSIONS
-
-from astropy.tests.helper import enable_deprecations_as_exceptions
+    try:
+        from pytest_astropy_header.display import PYTEST_HEADER_MODULES, TESTED_VERSIONS
+        ASTROPY_HEADER = True
+    except ImportError:
+        ASTROPY_HEADER = False
 
 
-# Uncomment the following line to treat all DeprecationWarnings as
-# exceptions
-# enable_deprecations_as_exceptions()    # noqa
+def pytest_configure(config):
 
-# Uncomment and customize the following lines to add/remove entries from
-# the list of packages for which version numbers are displayed when running
-# the tests. Making it pass for KeyError is essential in some cases when
-# the package uses other astropy affiliated packages.
-try:
-    PYTEST_HEADER_MODULES['Cython'] = 'Cython'    # noqa
-    PYTEST_HEADER_MODULES['Astropy'] = 'astropy'    # noqa
-    del PYTEST_HEADER_MODULES['h5py']    # noqa
-    del PYTEST_HEADER_MODULES['Pandas']    # noqa
-except (NameError, KeyError):  # NameError is needed to support Astropy < 1.0
-    pass
+    if ASTROPY_HEADER:
 
-# Uncomment the following lines to display the version number of the
-# package rather than the version number of Astropy in the top line when
-# running the tests.
-import os
+        config.option.astropy_header = True
 
-# This is to figure out the affiliated package version, rather than
-# using Astropy's
-try:
-    from .version import version, astropy_helpers_version
-except ImportError:
-    version = 'dev'
+        # Customize the following lines to add/remove entries from the list of
+        # packages for which version numbers are displayed when running the tests.
+        PYTEST_HEADER_MODULES['Cython'] = 'Cython'    # noqa
+        PYTEST_HEADER_MODULES['Astropy'] = 'astropy'    # noqa
+        del PYTEST_HEADER_MODULES['h5py']    # noqa
+        del PYTEST_HEADER_MODULES['Pandas']    # noqa
 
-packagename = os.path.basename(os.path.dirname(__file__))
-TESTED_VERSIONS[packagename] = version    # noqa
-TESTED_VERSIONS['astropy_helpers'] = astropy_helpers_version
+        from . import __version__
+        packagename = os.path.basename(os.path.dirname(__file__))
+        TESTED_VERSIONS[packagename] = __version__
+
+# Uncomment the last two lines in this block to treat all DeprecationWarnings as
+# exceptions. For Astropy v2.0 or later, there are 2 additional keywords,
+# as follow (although default should work for most cases).
+# To ignore some packages that produce deprecation warnings on import
+# (in addition to 'compiler', 'scipy', 'pygments', 'ipykernel', and
+# 'setuptools'), add:
+#     modules_to_ignore_on_import=['module_1', 'module_2']
+# To ignore some specific deprecation warning messages for Python version
+# MAJOR.MINOR or later, add:
+#     warnings_to_ignore_by_pyver={(MAJOR, MINOR): ['Message to ignore']}
+# from astropy.tests.helper import enable_deprecations_as_exceptions  # noqa
+# enable_deprecations_as_exceptions()
