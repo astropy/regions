c1a2d90df46441760c0d098d15e3422b433bb8f7
Larry Bradley <larry.bradley@gmail.com>
2021-05-11 22:25:53 -0400

    Add more BoundingBox tests

--- regions/core/tests/test_bounding_box.py
+++ regions/core/tests/test_bounding_box.py
@@ -1,9 +1,15 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
+"""
+Tests for the bounding_box module.
+"""
+
 from astropy.utils.exceptions import AstropyDeprecationWarning
 from numpy.testing import assert_allclose
 import pytest
 
 from ..bounding_box import BoundingBox
+from ..pixcoord import PixCoord
+from ...shapes import RectanglePixelRegion
 
 try:
     import matplotlib  # noqa
@@ -14,7 +20,6 @@ except ImportError:
 
 def test_bounding_box_init():
     bbox = BoundingBox(1, 10, 2, 20)
-
     assert bbox.ixmin == 1
     assert bbox.ixmax == 10
     assert bbox.iymin == 2
@@ -56,24 +61,23 @@ def test_bounding_box_from_float():
 
 def test_bounding_box_eq():
     bbox = BoundingBox(1, 10, 2, 20)
-    assert bbox == bbox
-
+    assert bbox == BoundingBox(1, 10, 2, 20)
     assert bbox != BoundingBox(9, 10, 2, 20)
     assert bbox != BoundingBox(1, 99, 2, 20)
     assert bbox != BoundingBox(1, 10, 9, 20)
     assert bbox != BoundingBox(1, 10, 2, 99)
 
+    with pytest.raises(TypeError):
+        assert bbox == (1, 10, 2, 20)
+
 
 def test_bounding_box_repr():
     bbox = BoundingBox(1, 10, 2, 20)
-
     assert repr(bbox) == 'BoundingBox(ixmin=1, ixmax=10, iymin=2, iymax=20)'
-    assert eval(repr(bbox)) == bbox
 
 
 def test_bounding_box_shape():
     bbox = BoundingBox(1, 10, 2, 20)
-
     assert bbox.shape == (18, 9)
 
 
@@ -104,11 +108,11 @@ def test_bounding_box_slices():
 
     with pytest.raises(ValueError):
         bbox = BoundingBox(-1, 10, 2, 20)
-        bbox.slices
+        _ = bbox.slices
 
     with pytest.raises(ValueError):
         bbox = BoundingBox(1, 10, -2, 20)
-        bbox.slices
+        _ = bbox.slices
 
 
 def test_bounding_box_extent():
@@ -119,11 +123,29 @@ def test_bounding_box_extent():
 @pytest.mark.skipif('not HAS_MATPLOTLIB')
 def test_bounding_box_as_artist():
     bbox = BoundingBox(1, 10, 2, 20)
-
     patch = bbox.as_artist()
+
     assert_allclose(patch.get_xy(), (0.5, 1.5))
     assert_allclose(patch.get_width(), 9)
     assert_allclose(patch.get_height(), 18)
+
+
+@pytest.mark.skipif('not HAS_MATPLOTLIB')
+def test_bounding_box_to_region():
+    bbox = BoundingBox(1, 10, 2, 20)
+    region = RectanglePixelRegion(PixCoord(5.0, 10.5), width=9., height=18.)
+    bbox_region = bbox.to_region()
+    assert_allclose(bbox_region.center.xy, region.center.xy)
+    assert bbox_region.width == region.width
+    assert bbox_region.height == region.height
+    assert bbox_region.angle == region.angle
+
+
+@pytest.mark.skipif('not HAS_MATPLOTLIB')
+def test_bounding_box_plot():
+    # TODO: check the content of the plot
+    bbox = BoundingBox(1, 10, 2, 20)
+    bbox.plot()
 
 
 def test_bounding_box_union():
