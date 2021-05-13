08985a7d6937341b97221a998a244a0c4cd141b9
Larry Bradley <larry.bradley@gmail.com>
2021-05-13 13:08:17 -0400

    Add docstrings to ShapeListRead/Write classes

--- regions/io/connect.py
+++ regions/io/connect.py
@@ -2,6 +2,24 @@ from astropy.io.registry import UnifiedReadWrite, read, write
 
 
 class ShapeListRead(UnifiedReadWrite):
+    """
+    Read and parse a region file and return as a `~regions.ShapeList`.
+
+    Parameters
+    ----------
+    *args : tuple, optional
+        Positional arguments passed through to data reader. If supplied
+        the first argument is typically the input filename.
+
+    **kwargs : dict, optional
+        Keyword arguments passed through to data reader.
+
+    Returns
+    -------
+    out : `~regions.ShapeList`
+        A `~regions.ShapeList` corresponding to the region file
+        contents.
+    """
 
     def __init__(self, instance, cls):
         super().__init__(instance, cls, 'read')
@@ -11,6 +29,19 @@ class ShapeListRead(UnifiedReadWrite):
 
 
 class ShapeListWrite(UnifiedReadWrite):
+    """
+    Write the `~regions.ShapeList` object to a file in the specified
+    format.
+
+    Parameters
+    ----------
+    *args : tuple, optional
+        Positional arguments passed through to data writer. If supplied,
+        the first argument is the output filename.
+
+    **kwargs : dict, optional
+        Keyword arguments passed through to data writer.
+    """
 
     def __init__(self, instance, cls):
         super().__init__(instance, cls, 'write')
