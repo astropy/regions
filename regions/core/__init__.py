a47f9f3f4d9fe7e351ee285cbc584b45c8319fb5
Larry Bradley <larry.bradley@gmail.com>
2021-06-10 15:02:24 -0400

    Add RegionList class

--- regions/core/__init__.py
+++ regions/core/__init__.py
@@ -10,3 +10,4 @@ from .core import *  # noqa
 from .mask import *  # noqa
 from .metadata import *  # noqa
 from .pixcoord import *  # noqa
+from .regionlist import *  # noqa
---
+++ regions/core/regionlist.py
@@ -0,0 +1,60 @@
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+"""
+This module provides a RegionList class.
+"""
+
+from .core import Region
+
+__all__ = ['RegionList']
+
+
+class RegionList:
+    """
+    Class to hold a list of `~regions.Region` objects.
+
+    Parameters
+    ----------
+    regions : list of `~region.Region`
+        The list of region objects.
+    """
+
+    def __init__(self, regions):
+        self.regions = regions
+
+    def __getitem__(self, index):
+        newregions = self.regions[index]
+        if isinstance(newregions, Region):  # one item
+            return newregions
+        else:
+            newcls = object.__new__(self.__class__)
+            newcls.regions = newregions
+            return newcls
+
+    def __len__(self):
+        return len(self.regions)
+
+    def append(self, item):
+        self.regions.append(item)
+
+    def extend(self, item):
+        self.regions.extend(item)
+
+    def insert(self, index, item):
+        self.regions.insert(index, item)
+
+    def reverse(self):
+        self.regions.reverse()
+
+    def pop(self, index=-1):
+        return self.regions.pop(index)
+
+    def copy(self):
+        newcls = object.__new__(self.__class__)
+        newcls.regions = self.regions.copy()
+        return newcls
+
+
+# use list API docs for these methods
+methods = ('append', 'extend', 'insert', 'reverse', 'pop', 'copy')
+for method in methods:
+    getattr(RegionList, method).__doc__ = getattr(list, method).__doc__
