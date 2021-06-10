14dabe40766fe1d551f6711262b40c2cc8068a07
Larry Bradley <larry.bradley@gmail.com>
2021-06-09 20:28:27 -0400

    Update FITS unified i/o

--- regions/io/fits/connect.py
+++ regions/io/fits/connect.py
@@ -4,18 +4,12 @@ from astropy.io import registry
 from astropy.io.fits.connect import is_fits
 
 from ..core import ShapeList
-from .read import read_fits_region
-from .write import write_fits_region
+from .read import read_fits
+from .write import write_fits
 
 __all__ = []
 
 
-# FIXME: write_fits_region has its first two arguments backwards as
-# compared to write_crtf and write_ds9
-def write_fits(regions, filename, *args, **kwargs):
-    return write_fits_region(filename, regions, *args, **kwargs)
-
-
-registry.register_reader('fits', ShapeList, read_fits_region)
+registry.register_reader('fits', ShapeList, read_fits)
 registry.register_writer('fits', ShapeList, write_fits)
 registry.register_identifier('fits', ShapeList, is_fits)
