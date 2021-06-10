c5d8f0e4bfaaa23b4ad3a2d02bb585656aa7ec57
Larry Bradley <larry.bradley@gmail.com>
2021-06-10 15:08:32 -0400

    Add unified read/write to RegionList

--- regions/core/regionlist.py
+++ regions/core/regionlist.py
@@ -3,9 +3,61 @@
 This module provides a RegionList class.
 """
 
+from astropy.io.registry import (UnifiedReadWrite, UnifiedReadWriteMethod,
+                                 read, write)
+
 from .core import Region
 
 __all__ = ['RegionList']
+
+
+class RegionListRead(UnifiedReadWrite):
+    """
+    Read and parse a region file and return as a `~regions.RegionList`.
+
+    Parameters
+    ----------
+    *args : tuple, optional
+        Positional arguments passed through to data reader. If supplied
+        the first argument is typically the input filename.
+
+    **kwargs : dict, optional
+        Keyword arguments passed through to data reader.
+
+    Returns
+    -------
+    out : `~regions.RegionList`
+        A `~regions.RegionList` corresponding to the region file
+        contents.
+    """
+
+    def __init__(self, instance, cls):
+        super().__init__(instance, cls, 'read')
+
+    def __call__(self, *args, **kwargs):
+        return read(self._cls, *args, **kwargs)
+
+
+class RegionListWrite(UnifiedReadWrite):
+    """
+    Write the `~regions.RegionList` object to a file in the specified
+    format.
+
+    Parameters
+    ----------
+    *args : tuple, optional
+        Positional arguments passed through to data writer. If supplied,
+        the first argument is the output filename.
+
+    **kwargs : dict, optional
+        Keyword arguments passed through to data writer.
+    """
+
+    def __init__(self, instance, cls):
+        super().__init__(instance, cls, 'write')
+
+    def __call__(self, *args, **kwargs):
+        write(self._instance, *args, **kwargs)
 
 
 class RegionList:
@@ -17,6 +69,10 @@ class RegionList:
     regions : list of `~region.Region`
         The list of region objects.
     """
+
+    # Unified I/O read and write methods
+    read = UnifiedReadWriteMethod(RegionListRead)
+    write = UnifiedReadWriteMethod(RegionListWrite)
 
     def __init__(self, regions):
         self.regions = regions
