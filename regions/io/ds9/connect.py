b9f12beb6f0bb6983274c2fd2fa99c83bdc2b258
Larry Bradley <larry.bradley@gmail.com>
2021-06-16 14:29:21 -0400

    Register identifier for ds9

--- regions/io/ds9/connect.py
+++ regions/io/ds9/connect.py
@@ -1,44 +1,46 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 
+from astropy.utils.data import get_readable_fileobj
+
+from ...core.registry import RegionsRegistry
+
 __all__ = []
 
-DS9_SIGNATURE = '# Region file format: DS9'
 
-
-def is_ds9(origin, path, fileobj, *args, **kwargs):  # noqa pylint: disable=unused-argument
+@RegionsRegistry.register('Regions', 'identify', 'ds9')
+def is_ds9(methodname, filepath):
     """
     Identify a DS9 region file.
 
     Parameters
     ----------
-    origin : {'read', 'write'}
-        A string identifying whether the file is to be opened for
-        reading or writing.
+    methodname : {'read', 'write'}
+        The method name called that needs auto-identification.
 
-    path : str
+    filepath : str
         The path to the file.
-
-    fileobj : file-like or `None`
-        An open file object to read the file's contents, or `None` if
-        the file could not be opened.
-
-    *args : tuple
-        Positional arguments for the ``read`` or ``write`` function.
-
-    **kwargs : dict
-        Keyword arguments for the ``read`` or ``write`` function.
 
     Returns
     -------
     result : bool
         Returns `True` if the given file is a DS9 region file.
     """
-    if fileobj is not None:
-        pos = fileobj.tell()
-        sig = fileobj.read(len(DS9_SIGNATURE))
-        fileobj.seek(pos)
-        return sig == DS9_SIGNATURE or sig == DS9_SIGNATURE.encode()
+    all_exten = ('.ds9', '.reg', '.ds9.gz', '.reg.gz')
+    exten = {'read': all_exten, 'write': all_exten[0:2]}
+
+    if methodname == 'write':
+        return filepath.lower().endswith(exten[methodname])
+
+    elif methodname == 'read':
+        if filepath.lower().endswith(exten[methodname]):
+            return True
+        else:
+            with get_readable_fileobj(filepath, encoding='binary') as fileobj:
+                signature = '# Region file format: DS9'
+                pos = fileobj.tell()
+                sig = fileobj.read(len(signature))
+                fileobj.seek(pos)
+                return sig == signature or sig == signature.encode()
+
     else:
-        return (path is not None
-                and path.lower().endswith(('.ds9', '.reg', '.ds9.gz',
-                                           '.reg.gz')))
+        return False
