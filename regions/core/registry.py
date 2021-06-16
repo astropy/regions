b964068aca61d3b8943411c9d74db5cbc5f8787c
Larry Bradley <larry.bradley@gmail.com>
2021-06-16 14:10:48 -0400

    Add auto-identification for writing files

--- regions/core/registry.py
+++ regions/core/registry.py
@@ -36,25 +36,30 @@ class RegionsRegistry:
                 if key[0] == classname and key[1] == 'identify']
 
     @classmethod
+    def get_format(cls, filename, classname, methodname):
+        format = None
+        identifiers = cls.get_identifiers(classname)
+        if identifiers:
+            for identifier in identifiers:
+                if cls.registry[identifier](methodname, filename):
+                    format = identifier[2]
+                    break  # finds the first valid filetype
+
+        if format is None:
+            raise IORegistryError('Format could not be identified based on '
+                                  'the file name or contents, please provide '
+                                  'a "format" argument.')
+        return format
+
+    @classmethod
     def read(cls, filename, classname, format=None, **kwargs):
         """
         Read in a regions file.
         """
         if format is None:
-            identifiers = cls.get_identifiers(classname)
-            if identifiers:
-                for identifier in identifiers:
-                    if cls.registry[identifier]('read', filename):
-                        format = identifier[2]
-                        break  # finds the first valid filetype
-
-            if format is None:
-                raise IORegistryError('Format could not be identified based '
-                                      'on the file name or contents, please '
-                                      'provide a "format" argument.')
-
+            format = cls.get_format(filename, classname, 'read')
         key = (classname, 'read', format)
-        return RegionsRegistry.registry[key](filename, **kwargs)
+        return cls.registry[key](filename, **kwargs)
 
     @classmethod
     def parse(cls, data, classname, format=None, **kwargs):
@@ -62,15 +67,17 @@ class RegionsRegistry:
         Parse a regions string or table.
         """
         key = (classname, 'parse', format)
-        return RegionsRegistry.registry[key](data, **kwargs)
+        return cls.registry[key](data, **kwargs)
 
     @classmethod
     def write(cls, regions, filename, classname, format=None, **kwargs):
         """
         Write to a regions file.
         """
+        if format is None:
+            format = cls.get_format(filename, classname, 'write')
         key = (classname, 'write', format)
-        return RegionsRegistry.registry[key](regions, filename, **kwargs)
+        return cls.registry[key](regions, filename, **kwargs)
 
     @classmethod
     def serialize(cls, regions, classname, format=None, **kwargs):
@@ -78,4 +85,4 @@ class RegionsRegistry:
         Serialize to a regions string or table.
         """
         key = (classname, 'serialize', format)
-        return RegionsRegistry.registry[key](regions, **kwargs)
+        return cls.registry[key](regions, **kwargs)
