a1d83c32a5107fa40d1c035e13132b84ac339243
Larry Bradley <larry.bradley@gmail.com>
2021-06-17 17:04:29 -0400

    Add Regions docstrings

--- regions/core/regions.py
+++ regions/core/regions.py
@@ -13,9 +13,12 @@ class Regions:
     """
     Class to hold a list of `~regions.Region` objects.
 
+    This class provides a unified I/O interface that supports reading,
+    writing, parsing, and serializing many region data formats.
+
     Parameters
     ----------
-    regions : list of `~region.Region`
+    regions : list of `~regions.Region`
         The list of region objects.
     """
 
@@ -34,22 +37,67 @@ class Regions:
     def __len__(self):
         return len(self.regions)
 
-    def append(self, item):
-        self.regions.append(item)
+    def append(self, region):
+        """
+        Append the region to the end of the list of regions.
 
-    def extend(self, item):
-        self.regions.extend(item)
+        Parameters
+        ----------
+        region : `~regions.Region`
+            The region to append.
+        """
+        self.regions.append(region)
 
-    def insert(self, index, item):
+    def extend(self, regions):
+        """
+        Extend the list of regions by appending elements from the
+        input regions.
+
+        Parameters
+        ----------
+        regions : list of `~regions.Region`
+            A list of regions to include.
+        """
+        self.regions.extend(iterable)
+
+    def insert(self, index, region):
+        """
+        Insert the region before index.
+
+        Parameters
+        ----------
+        index : int
+            The list index.
+        region : `~regions.Region`
+            The region to insert.
+        """
         self.regions.insert(index, item)
 
     def reverse(self):
+        """
+        Reverse the list of regions in place.
+        """
         self.regions.reverse()
 
     def pop(self, index=-1):
+        """
+        Remove and return the region at index.
+
+        Parameters
+        ----------
+        index : int, optional
+            The index of the region to remove.
+
+        Returns
+        -------
+        result : `~regions.Region`
+        """
         return self.regions.pop(index)
 
     def copy(self):
+        """
+        Return a shallow copy of this object.
+        """
         newcls = object.__new__(self.__class__)
         newcls.regions = self.regions.copy()
         return newcls
@@ -64,7 +112,41 @@ class Regions:
     @classmethod
     def read(cls, filename, format=None, cache=False, **kwargs):
         """
-        Read in a regions file.
+        Read and parse a region file and return as a Regions object.
+
+        This method allows reading a file in many supported data
+        formats, e.g.,::
+
+            >>> from regions import Regions
+            >>> reg1 = Regions.read('regions.reg', format='ds9')
+            >>> reg2 = Regions.read('regions.crtf', format='crtf')
+            >>> reg3 = Regions.read('regions.fits', format='fits')
+
+        A list of the available formats for `~regions.Regions` is
+        available using::
+
+            >>> Regions.get_formats()
+
+        Parameters
+        ----------
+        filename : str
+            The filename or URL of the file to read.
+
+        format : str, optional
+            The file format specifier.
+
+        cache : bool or 'update', optional
+            Whether to cache the contents of remote URLs. If 'update',
+            check the remote URL for a new version but store the result
+            in the cache.
+
+        **kwargs : dict, optional
+            Keyword arguments passed to the data reader.
+
+        Returns
+        -------
+        result : `~regions.Regions`
+            A `~regions.Regions` object containing the file contents.
         """
         return RegionsRegistry.read(filename, cls, format=format,
                                     cache=cache, **kwargs)
@@ -72,14 +154,68 @@ class Regions:
     @classmethod
     def parse(cls, data, format=None, **kwargs):
         """
-        Parse a regions string or table.
+        Parse a region string or table and return as a Regions object.
+
+        This method allows parsing region data in many supported data
+        formats, e.g.,::
+
+            >>> from regions import Regions
+            >>> reg1 = Regions.parse(regions_str, format='ds9')
+            >>> reg2 = Regions.parse(regions_str, format='crtf')
+            >>> reg3 = Regions.parse(regions_tbl, format='fits')
+
+        A list of the available formats for `~regions.Regions` is
+        available using::
+
+            >>> Regions.get_formats()
+
+        Parameters
+        ----------
+        data : str or `~astropy.table.Table`
+            The region data to parse.
+
+        format : str, optional
+            The file format specifier.
+
+        **kwargs : dict, optional
+            Keyword arguments passed to the data parser.
+
+        Returns
+        -------
+        result : `~regions.Regions`
+            A `~regions.Regions` object containing the data contents.
         """
         return RegionsRegistry.parse(data, cls, format=format,
                                      **kwargs)
 
     def write(self, filename, format=None, **kwargs):
         """
-        Write to a regions file.
+        Write the regions to a region file in the specified format.
+
+        This method allows writing a file in many supported data
+        formats, e.g.,::
+
+            >>> from regions import Regions
+            >>> reg = Regions.read('regions.reg', format='ds9')
+            >>> reg.write('new_regions.reg', format='ds9')
+            >>> reg.write('new_regions.crtf', format='crtf')
+            >>> reg.write('new_regions.fits', format='fits')
+
+        A list of the available formats for `~regions.Regions` is
+        available using::
+
+            >>> Regions.get_formats()
+
+        Parameters
+        ----------
+        filename : str
+            The filename or URL of the file to write.
+
+        format : str, optional
+            The file format specifier.
+
+        **kwargs : dict, optional
+            Keyword arguments passed to the data writer.
         """
         return RegionsRegistry.write(self.regions, filename,
                                      self.__class__, format=format,
@@ -87,13 +223,29 @@ class Regions:
 
     def serialize(self, format=None, **kwargs):
         """
-        Serialize to a regions string or table.
+        Serialize the regions to a region string or table.
+
+        This method allows serializing regions in many supported data
+        formats, e.g.,::
+
+            >>> from regions import Regions
+            >>> reg = Regions.read('regions.reg', format='ds9')
+            >>> reg1_str = reg.serialize(format='ds9')
+            >>> reg2_str = reg.serialize(format='crtf')
+            >>> reg3_tbl = reg.serialize(format='fits')
+
+        A list of the available formats for `~regions.Regions` is
+        available using::
+
+            >>> Regions.get_formats()
+
+        Parameters
+        ----------
+        format : str, optional
+            The file format specifier.
+
+        **kwargs : dict, optional
+            Keyword arguments passed to the data serializer.
         """
         return RegionsRegistry.serialize(self.regions, self.__class__,
                                          format=format, **kwargs)
-
-
-# reuse the `list` API docs for these methods
-methods = ('append', 'extend', 'insert', 'reverse', 'pop', 'copy')
-for method in methods:
-    getattr(Regions, method).__doc__ = getattr(list, method).__doc__
