afdda78d94771c691fbe21be6b7915587311af1b
Larry Bradley <larry.bradley@gmail.com>
2021-07-23 13:47:34 -0400

    Remove deprecated fits io functions

--- regions/io/fits/write.py
+++ regions/io/fits/write.py
@@ -1,13 +1,12 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 
 from astropy.io import fits
-from astropy.utils.decorators import deprecated
 
 from ...core import Region, Regions
 from ...core.registry import RegionsRegistry
 from ..core import _to_shape_list, SkyRegion
 
-__all__ = ['write_fits', 'fits_region_objects_to_table']
+__all__ = []
 
 
 @RegionsRegistry.register(Region, 'serialize', 'fits')
@@ -19,24 +18,6 @@ def _serialize_fits(regions):
 
     shape_list = _to_shape_list(regions, coordinate_system='image')
     return shape_list.to_fits()
-
-
-@deprecated('0.5', alternative='`regions.Regions.serialize`')
-def fits_region_objects_to_table(regions):
-    """
-    Convert a list of `~regions.Region` objects to a FITS region table.
-
-    Parameters
-    ----------
-    regions : list
-        A list of `regions.Region` objects.
-
-    Returns
-    -------
-    region_string : `~astropy.table.Table`
-        A FITS region table.
-    """
-    return _serialize_fits(regions)
 
 
 @RegionsRegistry.register(Region, 'write', 'fits')
@@ -64,51 +45,3 @@ def _write_fits(regions, filename, header=None, overwrite=False):
     output = _serialize_fits(regions)
     bin_table = fits.BinTableHDU(data=output, header=header)
     bin_table.writeto(filename, overwrite=overwrite)
-
-
-@deprecated('0.5', alternative='`regions.Regions.write`')
-def write_fits_region(filename, regions, header=None, overwrite=False):
-    """
-    Convert a list of `~regions.Region` to a FITS region table and write
-    to a file.
-
-    Parameters
-    ----------
-    filename : str
-        The filename in which the table is to be written.
-
-    regions : list
-        A list of `~regions.Region` objects.
-
-    header : `~astropy.io.fits.Header`, optional
-        The FITS header.
-
-    overwrite : bool, optional
-        If True, overwrite the output file if it exists. Raises an
-        `OSError` if False and the output file exists. Default is False.
-    """
-    _write_fits(regions, filename, header=header, overwrite=overwrite)
-
-
-@deprecated('0.5', alternative='`regions.Regions.write`')
-def write_fits(filename, regions, header=None, overwrite=False):
-    """
-    Convert a list of `~regions.Region` to a FITS region table and write
-    to a file.
-
-    Parameters
-    ----------
-    filename : str
-        The filename in which the table is to be written.
-
-    regions : list
-        A list of `~regions.Region` objects.
-
-    header : `~astropy.io.fits.Header`, optional
-        The FITS header.
-
-    overwrite : bool, optional
-        If True, overwrite the output file if it exists. Raises an
-        `OSError` if False and the output file exists. Default is False.
-    """
-    _write_fits(regions, filename, header=header, overwrite=overwrite)
