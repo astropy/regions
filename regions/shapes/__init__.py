4b5a7f797882ea8b5fcb19980d7e6703c0be9381
Sedona Price <sprice@stsci.edu>
2025-07-28 18:24:15 -0400

    Add spherical range region

--- regions/shapes/__init__.py
+++ regions/shapes/__init__.py
@@ -10,5 +10,6 @@ from .line import *  # noqa: F401, F403
 from .lune import *  # noqa: F401, F403
 from .point import *  # noqa: F401, F403
 from .polygon import *  # noqa: F401, F403
+from .range import *  # noqa: F401, F403
 from .rectangle import *  # noqa: F401, F403
 from .text import *  # noqa: F401, F403
