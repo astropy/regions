1004600cb0d06374faf34fd48a76f161a7665537
Larry Bradley <larry.bradley@gmail.com>
2025-11-24 13:15:28 -0500

    Resolve conflict with main in test_api.py
    Cherry pick of e2b43d7 into fork branch, with edits
    to bring fork branch changes into consistency with the
    new pytest parameterization usage.
    
    Original commit:
    e2b43d7: Fix pytest10 non-Collection iterable deprecation

--- regions/shapes/tests/test_api.py
+++ regions/shapes/tests/test_api.py
@@ -110,11 +110,8 @@ def test_pix_to_sky(region):
         pytest.xfail()
 
 
-@pytest.mark.parametrize(('region', 'include_dist'),
-                         itertools.product(
-                             PIXEL_REGIONS,
-                             INCLUDE_BOUNDARY_DISTORTIONS),
-                         ids=ids_func)
+@pytest.mark.parametrize('region', PIXEL_REGIONS, ids=ids_func)
+@pytest.mark.parametrize('include_dist', INCLUDE_BOUNDARY_DISTORTIONS, ids=ids_func)
 def test_pix_to_spherical_sky(region, include_dist):
     # TODO: remove expected failures when implemented
     # Expected failure:
@@ -175,11 +172,8 @@ def test_sky_to_pix(region):
     assert isinstance(pix_region, PixelRegion)
 
 
-@pytest.mark.parametrize(('region', 'include_dist'),
-                         itertools.product(
-                             SKY_REGIONS,
-                             INCLUDE_BOUNDARY_DISTORTIONS),
-                         ids=ids_func)
+@pytest.mark.parametrize('region', SKY_REGIONS, ids=ids_func)
+@pytest.mark.parametrize('include_dist', INCLUDE_BOUNDARY_DISTORTIONS, ids=ids_func)
 def test_sky_to_spherical_sky(region, include_dist):
     # TODO: remove expected failures when implemented
     # Expected failure:
@@ -213,11 +207,8 @@ def test_sky_to_spherical_sky(region, include_dist):
         assert isinstance(sph_sky_region, SphericalSkyRegion)
 
 
-@pytest.mark.parametrize(('region', 'include_dist'),
-                         itertools.product(
-                             SPHERICAL_SKY_REGIONS,
-                             INCLUDE_BOUNDARY_DISTORTIONS),
-                         ids=ids_func)
+@pytest.mark.parametrize('region', SPHERICAL_SKY_REGIONS, ids=ids_func)
+@pytest.mark.parametrize('include_dist', INCLUDE_BOUNDARY_DISTORTIONS, ids=ids_func)
 def test_spherical_sky_to_sky(region, include_dist):
     # TODO: remove excepted failure for Lune+distortion once Lune handling is implemented
     if isinstance(region, WholeSphericalSkyRegion):
@@ -255,11 +246,8 @@ def test_spherical_sky_to_sky(region, include_dist):
         assert isinstance(sky_region, SkyRegion)
 
 
-@pytest.mark.parametrize(('region', 'include_dist'),
-                         itertools.product(
-                             SPHERICAL_SKY_REGIONS,
-                             INCLUDE_BOUNDARY_DISTORTIONS),
-                         ids=ids_func)
+@pytest.mark.parametrize('region', SPHERICAL_SKY_REGIONS, ids=ids_func)
+@pytest.mark.parametrize('include_dist', INCLUDE_BOUNDARY_DISTORTIONS, ids=ids_func)
 def test_spherical_sky_to_pix(region, include_dist):
     # TODO: remove excepted failure for Lune+distortion once Lune handling is implemented
     if isinstance(region, WholeSphericalSkyRegion):
