4168ac53dacb297343de023d004f5838733c8bdf
Larry Bradley <larry.bradley@gmail.com>
2026-08-03 13:09:24 -0400

    Fix fontstyle assignment

--- regions/core/metadata.py
+++ regions/core/metadata.py
@@ -252,6 +252,6 @@ class RegionVisual(Meta):
             kwargs.pop(key, None)
 
         if 'fontstyle' in kwargs:
-            kwargs['fontstyle'].replace('roman', 'normal')
+            kwargs['fontstyle'] = kwargs['fontstyle'].replace('roman', 'normal')
 
         return kwargs
